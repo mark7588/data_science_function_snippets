86bc999e342a1de5c16bc2aa94eefc516be3c085
TaeJoon Kim <taejoonkim0119@gmail.com>
2024-09-21 21:19:38 -0400

    first commit

---
+++ data_visualization.py
@@ -0,0 +1,27 @@
+# Display scatter plot between two variables x and y 
+def display_scatter_plot(x, y, x_label, y_label):
+    plt.scatter(x, y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(f"Scatter plot of {x_label} vs {y_label}")
+    plt.show()
+
+# Display scatter plot with trend line between two variables x and y 
+def display_scatter_plot_trend_line(x, y, x_label, y_label):
+    plt.scatter(x, y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    
+    slope, intercept = np.polyfit(x, y, 1)
+    plt.plot(x, slope * x + intercept, color='red', label='Trend Line')
+
+    plt.title(f"Scatter plot of {x_label} vs {y_label}")
+    plt.show()
+
+# Extract quantitative field from all of the fields in the table 
+def extract_numeric_data (df):
+    categorical_fields = [col for col in df.columns if df[col].dtype == 'object']
+    numeric_fields = [col for col in df.columns if df[col].dtype != 'object']
+    return numric_fields 
+
+
